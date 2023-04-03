443ddd15263c9c703cc69c1c556958b34b958b27
mokwongame <ictplusud@gmail.com>
2023-04-03 17:27:49 +0900

    MyRainbowBallGame에 공 제어 기능 추가

--- MyRainbowBallGame/Assets/BallWithInput.cs
+++ MyRainbowBallGame/Assets/BallWithInput.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BallWithInput : MonoBehaviour
 {
     public bool isKinematic = true;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +22,9 @@ public class BallWithInput : MonoBehaviour
             isKinematic = !isKinematic;
             rb.isKinematic = isKinematic; // !: not(논리 부정)
         }
+        // 공을 이동
+        float xoff = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime; // Input.GetAxisRaw("Horizontal"): 수평 방향(좌우) 입력 얻기; -1, 1, 0
+        float zoff = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime; // Input.GetAxisRaw("Horizontal"): 수직 방향(상하) 입력 얻기; -1, 1, 0
+        gameObject.transform.Translate(xoff, 0.0f, zoff); // 물체의 병진 운동(물체 중심이 움직이는 운동
     }
 }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "MyRainbowBallGame 프로젝트 추가"
+git commit -m "MyRainbowBallGame에 공 제어 기능 추가"
 git push -u origin master
